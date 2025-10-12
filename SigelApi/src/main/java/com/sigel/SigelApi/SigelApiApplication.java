61ed4d1a60df90ff1d36261cb1aec2325048dba2
I.Martinnz <irvingret3948@gmail.com>
2025-10-11 21:08:22 -0600

    Agregando la API

---
+++ SigelApi/src/main/java/com/sigel/SigelApi/SigelApiApplication.java
@@ -0,0 +1,13 @@
+package com.sigel.SigelApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigelApiApplication.class, args);
+	}
+
+}
---
+++ SigelApi/src/test/java/com/sigel/SigelApi/SigelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigel.SigelApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
